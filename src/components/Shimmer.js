d2e92cb0674162ad84ad0675f7651469c72ee2f4
GCell-droid <niteshparihar104@gmail.com>
2025-04-28 19:49:15 +0530

    Updated shimmmer

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,5 +1,5 @@
 const Shimmer = () => {
-    return (<div className="shimmer-cont flex flex-wrap">
+    return (<div className="shimmer-cont flex flex-wrap justify-center">
         <div className="shimmer-cardflex  flex-col w-64 h-80 items-center p-2 font-normal  overflow-hidden m-2 bg-gray-100 shadow-m rounded-md hover:bg-gray-200"></div>
         <div className="shimmer-cardflex  flex-col w-64 h-80 items-center p-2 font-normal  overflow-hidden m-2 bg-gray-100 shadow-m rounded-md hover:bg-gray-200"></div>
         <div className="shimmer-cardflex  flex-col w-64 h-80 items-center p-2 font-normal  overflow-hidden m-2 bg-gray-100 shadow-m rounded-md hover:bg-gray-200"></div>
